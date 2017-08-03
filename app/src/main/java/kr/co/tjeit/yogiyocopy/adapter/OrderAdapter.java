edf4839b81fb70a75b6d7fd08fe446c6c9364046
cho881020 <cho881020@gmail.com>
2017-08-03 18:19:48 +0900

    프로필 화면 추가

--- app/src/main/java/kr/co/tjeit/yogiyocopy/adapter/OrderAdapter.java
+++ app/src/main/java/kr/co/tjeit/yogiyocopy/adapter/OrderAdapter.java
@@ -7,8 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import kr.co.tjeit.yogiyocopy.R;
 import kr.co.tjeit.yogiyocopy.data.OrderData;
@@ -41,6 +44,26 @@ public class OrderAdapter extends ArrayAdapter<OrderData> {
         if ( row == null) {
             row = inf.inflate(R.layout.order_list_item, null);
         }
+
+        OrderData data = mList.get(position);
+
+        TextView storeNameTxt = (TextView) row.findViewById(R.id.storeNameTxt);
+        storeNameTxt.setText(data.getOrderStore().getStoreName());
+
+        TextView dateTxt = (TextView) row.findViewById(R.id.dateTxt);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 M월 dd일");
+        dateTxt.setText(sdf.format(data.getOrderDate().getTime()));
+
+        TextView locationTxt = (TextView) row.findViewById(R.id.locationTxt);
+        locationTxt.setText(data.getLocation());
+
+        TextView amountTxt = (TextView) row.findViewById(R.id.amountTxt);
+        String amountStr = String.format(Locale.KOREA, "%,d 원", data.getCost());
+        amountTxt.setText(amountStr);
+        // DecimalFormater
+        // NumberFormater
+        // String.format => 채택 => %d 10진수. %,d 세자리 끊어주기
 
         return row;
     }
